3780785f8bb1c617b3bb0f1db2c13c1a9d07ac92
Douglas Tomazini <douglastomazini@gmail.com>
2022-04-04 21:33:14 -0300

    Update backup_semsql.sh

--- backup_semsql.sh
+++ backup_semsql.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 
-mysql_host="localhost"
-mysql_user="user"
-mysql_passwd="password"
-base="base"
-dominio="domain"
-bkp_banco="/backup/domain.sql"
+dominio="douglastomazini.tk"
 data='date +%d_%m_%y'
+user="douglastomazini"
 
 #--------------------------------FTP---------------------
 
+mkdir -p backup
+
 echo "Iniciando compactação do FTP em: " $($data) >> /backup/log.txt
-tar -czf /backup/$($data)domain.tar.gz /home/domain/
+tar -czf /backup/$($data)$dominio.tar.gz /home/$user/
 echo "Compactação de FTP realizada com sucesso em: " $($data) >> /backup/log.txt
 
 
