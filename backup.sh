d77cc636f58ad70e097cae535dd197aec2afa22b
DouglasTomazini <douglastomazini@gmail.com>
2022-04-25 21:23:23 -0300

    update condições para escrever nos logs.
    Adicionadas as condições para o script escrever no arquivo de log, dependendo se o backup for bem ou mal sucedido.

--- backup.sh
+++ backup.sh
@@ -5,26 +5,33 @@ mysql_user="user"
 mysql_passwd="password"
 base="base"
 dominio="domain"
-bkp_banco="/backup/domain.sql"
 data='date +%d_%m_%y'
 #--------------------------------mysql-------------------
 
 mkdir -p backup
 
 echo "Iniciando backup mysql em: " $($data) >> /backup/log.txt
-mysqldump -h$mysql_host -u$mysql_user -p$mysql_passwd $base > /backup/douglastomazini.sql
 
-        tar -czf /backup/$($data)domain.sql.tar.gz /backup/domain.sql
-        echo "compactação do  sql concluída em: " $($data)>>/backup/log.txt
-        rm /backup/domain.sql
+if mysqldump -h$mysql_host -u$mysql_user -p$mysql_passwd $base >> /backup/douglastomazini.sql ; then
+    echo "Exportação mysql realizada com sucesso" >> /backup/douglastomazini.sql
+else
+    echo "Erro ao realizar a exportação mysql, verificar os logs em /backup/log.txt Erro gerado em:" $($data) >> /backup/log.txt
+fi    
 
-
+if tar -czf /backup/$($data)$dominio.sql.tar.gz /backup/$dominio.sql ; then
+    echo "compactação do  sql concluída em: " $($data)>>/backup/log.txt
+    rm /backup/$dominio.sql
+else
+    echo "erro na compactação mysql, verificar os logs em /backup/log.txt Erro gerado em:" $($data)>> /backup/log.txt
+fi
 #--------------------------------FTP---------------------
 
 echo "Iniciando compactação do FTP em: " $($data) >> /backup/log.txt
-tar -czf /backup/$($data)domain.tar.gz /home/domain/
-echo "Compactação de FTP realizada com sucesso em: " $($data) >> /backup/log.txt
-
+if tar -czf /backup/$($data)$dominio.tar.gz /home/$dominio/ ; then 
+    echo "Compactação de FTP realizada com sucesso em: " $($data) >> /backup/log.txt
+else
+    echo "Erro na compactação, verificar os logs em /backup/log.txt Erro gerado em:" $($data) >> /backup/log.txt
+fi
 
 #----------------------------removendo os backups antigos-----------
 
